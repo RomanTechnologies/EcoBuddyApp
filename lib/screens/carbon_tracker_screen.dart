84f4a9e8b0cd0af41281353bdc96490f2b779f9a
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-18 23:34:25 +0000

    Added carbon tracker screen

---
+++ lib/screens/carbon_tracker_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../config/theme/my_theme.dart';
+
+class CarbonTrackerScreen extends GetWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text('Carbon Footprint Tracker'),
+      ),
+    );
+  }
+}
